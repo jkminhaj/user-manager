6507606d27bf24e623a7c79e2722b52e1962579b
Minhajul Islam <mmminhaj221@gmail.com>
2025-11-14 20:26:12 +0600

    added database config

--- src/user_manager/User_Manager.java
+++ src/user_manager/User_Manager.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package user_manager;
+import java.sql.* ;
 
-/**
- *
- * @author Hi
- */
+
 public class User_Manager {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        String dbUrl = "jdbc:mysql://localhost:3307/users_db";
+        String userName = "root";
+        String password = "";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection cnn = DriverManager.getConnection(dbUrl,userName,password);
+            if(cnn!=null){
+                System.out.println("Database Connected");
+            }
+//            responsible for executing queries on table
+            Statement smt = cnn.createStatement();
+            // executeUpdate() is used for INSERT, UPDATE,DELETE statements.It returns number of rows
+            String query = "";
+            int result = smt.executeUpdate(query);
+            if(result>0)System.out.println("Query executed successfully");
+            else System.out.println("Something went wrong with query");
+            cnn.close();
+            
+        }
+        catch(SQLException ex){
+            System.out.println("SQL Error : "+ ex);
+        }
+        catch(Exception ex){
+            System.out.println("Error : "+ ex);
+        }
     }
     
 }
